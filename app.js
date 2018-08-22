3e453574c62d92e0414d7176c888d334a0c08b49
Sam <samvanerpersroijaards@gmail.com>
2018-08-22 16:14:50 +1200

    added Celcius and Newton scale

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-// Today's forecast
-const kelvin = 293
+// Change this constant to calculate your temperature
+const kelvin = 0
 
 // Converts forecast to celcius
 var celcius = kelvin - 273
+
+// Converts to Newton
+var newton = celcius * (33/100)
+
+// Rounds down to whole number
+newton = Math.floor(newton)
 
 // Converts to farenheit
 var farenheit = celcius * (9/5) + 32
@@ -11,4 +17,9 @@ var farenheit = celcius * (9/5) + 32
 farenheit = Math.floor(farenheit)
 
 
+
 console.log(`The temperature is ${farenheit} degrees Farenheit.`)
+
+console.log(`The temperature is ${celcius} degrees Celcius.`)
+
+console.log(`The temperature is ${newton} on the Newton scale.`)
